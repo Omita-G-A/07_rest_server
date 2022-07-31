191eef1d9da82127e3abf1529838c8e69fd41824
Omi <omienred@gmail.com>
2022-07-31 19:29:55 +0200

    primer comit

---
+++ models/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    // constructor
+    constructor() {
+        this.app = express(); //crear una app de express como propiedad
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // middlewares
+        this.middlewares()
+
+        // rutas
+        this.routes();
+
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors());
+
+        // lectura y parseo del body
+        this.app.use(express.json());
+        // directorio public
+        this.app.use(express.static('public'));
+    }
+
+
+    routes() {
+        
+        this.app.use(this.usuariosPath, require('../routes/users.routes'));
+        
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        });
+    }
+
+}
+
+
+module.exports = Server;
---
+++ routes/users.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const { usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete } = require('../controllers/user.controllers');
+
+const router = Router();
+
+router.get('/', usuariosGet);
+
+router.post('/', usuariosPost);
+
+router.put('/:id', usuariosPut);
+
+router.patch('/', usuariosPatch);
+
+router.delete('/:id', usuariosDelete);
+
+
+module.exports = router;
